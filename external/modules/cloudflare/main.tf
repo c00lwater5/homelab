fc030d2018cf7f561d443bd1c92103a148e3e4e5
mukhtaar <mukh_@hotmail.com>
2026-09-17 15:17:05 +0200

    makefile run

--- external/modules/cloudflare/main.tf
+++ external/modules/cloudflare/main.tf
@@ -1,5 +1,5 @@
 data "cloudflare_zone" "zone" {
-  name = "khuedoan.com"
+  name = "mukhtaar.org"
 }
 
 data "cloudflare_api_token_permission_groups" "all" {}
--- platform/gitea/files/config/go.mod
+++ platform/gitea/files/config/go.mod
@@ -1,4 +1,4 @@
-module git.khuedoan.com/khuedoan/homelab/gitea/config
+module git.mukhtaar.org/khuedoan/homelab/gitea/config
 
 go 1.19
 
