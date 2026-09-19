62f3e784853ea32fa2b2881d2c9f9162b9047a44
c00lwater5 <mukh_@hotmail.com>
2026-09-19 16:43:49 +0200

    Config updates

--- external/main.tf
+++ external/main.tf
@@ -1,8 +1,7 @@
 module "cloudflare" {
   source                = "./modules/cloudflare"
   cloudflare_account_id = var.cloudflare_account_id
-  cloudflare_email      = var.cloudflare_email
-  cloudflare_api_key    = var.cloudflare_api_key
+  cloudflare_api_token = var.cloudflare_api_token
 }
 
 module "ntfy" {
